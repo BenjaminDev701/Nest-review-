69714ce9776414bfb14a994a525be9ee486b98e9
BenjaminDev701 <learncodigo777@gmail.com>
2026-09-09 20:54:22 -0600

    Feat:Implementacion y configuracion de JWT Y PassportStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,38 +6,42 @@ import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JWTStrategy } from './strategies/jwt.strategy';
 
 @Module({
+
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User]),
+  providers: [AuthService, JWTStrategy],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([User]),
 
-  /* 
-  Configuración de passport para el manejo de autenticación 
-  */
-  PassportModule.register({ defaultStrategy: "jwt" }),
+    /* 
+    Configuración de passport para el manejo de autenticación 
+    */
+    PassportModule.register({ defaultStrategy: "jwt" }),
 
-  /* 
-  Configuración del módulo JWT 
-  */
-  JwtModule.registerAsync({
-    //*permite inyectar el config service para acceder a las variables de entorno
-    imports: [ConfigModule],
-    //*inyecta el config service para acceder a las variables de entorno
-    inject: [ConfigService],
-    //*useFactory se usa para configuraciones asincronas haciendo que se ejecute primero antes de iniciar la aplicacion.
-    useFactory: (configService: ConfigService) => {
-      return {
-        //*la palabra secreta del JWT
-        secret: configService.get("SECRET_WORD"),
-        signOptions: {
-          //*tiempo de expiracion del token
-          expiresIn: "2h"
+    /* 
+    Configuración del módulo JWT 
+    */
+    JwtModule.registerAsync({
+      //*permite inyectar el config service para acceder a las variables de entorno
+      imports: [ConfigModule],
+      //*inyecta el config service para acceder a las variables de entorno
+      inject: [ConfigService],
+      //*useFactory se usa para configuraciones asincronas haciendo que se ejecute primero antes de iniciar la aplicacion.
+      useFactory: (configService: ConfigService) => {
+        return {
+          //*la palabra secreta del JWT
+          secret: configService.get("SECRET_WORD"),
+          signOptions: {
+            //*tiempo de expiracion del token
+            expiresIn: "2h"
+          }
         }
       }
-    }
-  })
+    })
   ],
-  exports: [TypeOrmModule]
+  exports: [TypeOrmModule, JWTStrategy, PassportModule, JwtModule]
 })
 export class AuthModule { }
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,16 +1,39 @@
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { ExtractJwt, Strategy } from "passport-jwt";
 import { User } from "../entities/user.entity";
 import { JwtPaylaod } from "../interfaces/jwt-payload.interface";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { ConfigService } from "@nestjs/config";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 
+
+//*hace que esto lo podamos enlazar con otro modulo
+@Injectable()
 export class JWTStrategy extends PassportStrategy(Strategy) {
 
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+        configService: ConfigService
+    ) {
+        super({
+            secretOrKey: configService.get("SECRET_WORD") as string,
+            //*le dice a passport que el token va a venir en el header como Bearer Token
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+        })
+    }
 
     async validate(payload: JwtPaylaod): Promise<User> {
 
         const { email } = payload;
 
-        return;
+        const user = await this.userRepository.findOneBy({ email })
+        if (!user) throw new UnauthorizedException("Token no valid")
+
+        if (!user.isActive) throw new UnauthorizedException("is not active")
+
+        return user;
     }
 
 }
