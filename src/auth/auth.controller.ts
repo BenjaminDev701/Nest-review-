94d6b37e2681dedc4714317962d94747c5b0f295
BenjaminDev701 <learncodigo777@gmail.com>
2026-09-13 18:23:38 -0600

    Feat:Agregar UserRoleGuard y metadatos de roles

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
@@ -6,6 +6,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './decorators/get-user-decorator';
 import { User } from './entities/user.entity';
 import { RawHeaders } from './decorators/get-rawHeaders-decorator';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -43,4 +44,16 @@ export class AuthController {
     }
   }
 
+
+  @Get("private2")
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  @SetMetadata("roles", ["admin", "super-user",])
+  privateRoute2(
+    @GetUser() user: User
+  ) {
+    return {
+      ok: true,
+      user
+    }
+  }
 }
